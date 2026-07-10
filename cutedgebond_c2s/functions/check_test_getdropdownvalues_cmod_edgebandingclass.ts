f39ba24a876db3421e9bc16984e48d8175e3ad80
calin.chis@cadrantechnologies.eu <calin.chis@cadrantechnologies.eu>
2026-07-10 08:12:18 +0000

    Added check for attributes: cmod_EdgeBandingClass, mod_SubArticleName

---
+++ cutedgebond_c2s/functions/check_test_getdropdownvalues_cmod_edgebandingclass.ts
@@ -0,0 +1,3 @@
+check_test_getDropDownValues_cmod_EdgeBandingClass(attr: Checks.Itest_Attributes, selections: SelectionEntry_cmod_EdgeBandingClass[]): Checks.CheckDropDownEntry[]{
+    return [];
+}
---
+++ cutedgebond_c2s/functions/check_test_getdropdownvalues_mod_subarticlename.ts
@@ -0,0 +1,3 @@
+check_test_getDropDownValues_mod_SubArticleName(attr: Checks.Itest_Attributes, selections: SelectionEntry_mod_SubArticleName[]): Checks.CheckDropDownEntry[]{
+    return [];
+}
