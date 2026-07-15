d48989a0d9c4487895c168a65d41281a0568a7f8
cristian.prelipcean@cadrantechnologies.eu <cristian.prelipcean@cadrantechnologies.eu>
2026-07-15 13:52:25 +0000

    Function 'check_AB_getDropDownValues_B' changed.

--- cristi_ts/functions/check_ab_getdropdownvalues_b.ts
+++ cristi_ts/functions/check_ab_getdropdownvalues_b.ts
@@ -1,3 +1,14 @@
 check_AB_getDropDownValues_B(attr: Checks.IAB_Attributes, selections: SelectionEntry_B[]): Checks.CheckDropDownEntry[]{
-    return [];
+    if (attr.A === 'A1') {
+        return [
+            { value: selections[0].value, kind: Checks.CheckDropDownEntryKind.Normal }, 
+            { value: selections[1].value, kind: Checks.CheckDropDownEntryKind.Invalid }, 
+            { value: selections[2].value, kind: Checks.CheckDropDownEntryKind.Invalid }, 
+        ]
+    }
+    return [
+        { value: selections[0].value, kind: Checks.CheckDropDownEntryKind.Invalid }, 
+        { value: selections[1].value, kind: Checks.CheckDropDownEntryKind.Normal }, 
+        { value: selections[2].value, kind: Checks.CheckDropDownEntryKind.Invalid }, 
+    ]
 }
