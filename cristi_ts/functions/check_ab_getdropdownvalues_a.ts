fb01a980f2a5c9b5dfddf07a455c86a67dcced37
cristian.prelipcean@cadrantechnologies.eu <cristian.prelipcean@cadrantechnologies.eu>
2026-07-17 12:07:48 +0000

    Deleted check with id: 9

--- cristi_ts/functions/check_ab_getdropdownvalues_a.ts
+++
@@ -1,7 +0,0 @@
-check_AB_getDropDownValues_A(attr: Checks.IAB_Attributes, selections: SelectionEntry_A[]): Checks.CheckDropDownEntry[]{
-    return [
-        {value: selections[0].value, kind: Checks.CheckDropDownEntryKind.Normal}, 
-        {value: selections[1].value, kind: Checks.CheckDropDownEntryKind.Normal}, 
-        {value: selections[2].value, kind: Checks.CheckDropDownEntryKind.Invalid}, 
-    ]
-}
