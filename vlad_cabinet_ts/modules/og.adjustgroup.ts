ded6ef51a3dd685998df853ab1fa5fad517e7fe7
Vlad.Cobzaru@cadrantechnologies.eu <Vlad.Cobzaru@cadrantechnologies.eu>
2026-07-20 06:58:42 +0000

    Updated AdjustGroupScript script for module 'og'

--- vlad_cabinet_ts/modules/og.adjustgroup.ts
+++ vlad_cabinet_ts/modules/og.adjustgroup.ts
@@ -1,1 +1,15 @@
-  logInfo("adjust with success");
+  logInfo("adjust with success from group orchestrator.");
+  const newArticle = this.createArticle('tests');
+  if (newArticle) {
+    this.addRoots(newArticle);
+
+    let r1 = newArticle[0].root;
+    if (r1 instanceof OD_M_shape01) {
+      r1.height = 1548;
+    }
+
+    this.addDocking(this.roots[0].root, Dock.LeftBottom, r1, Dock.RightBottom);
+  }
+  else {
+    logError("article not found");
+  }
