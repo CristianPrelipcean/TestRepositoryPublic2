4c3c2703dd541edcd1ea05de1fd76149b437e251
heiko.reichstetter@homag.com <heiko.reichstetter@homag.com>
2026-07-20 07:28:55 +0000

    Updated AdjustGroupScript script for module 'go'

--- cabinet_ts/modules/go.adjustgroup.ts
+++ cabinet_ts/modules/go.adjustgroup.ts
@@ -2,4 +2,16 @@
   logInfo("x:" + this.getArticlePos().x);
   logInfo("y:" + this.getArticlePos().y);
   logInfo("z:" + this.getArticlePos().z);
-  logInfo("rotationY:" + this.getArticlePos().rotationY);
+  logInfo("rotationY:" + this.getArticlePos().rotationY);
+
+  if (this.roots.length > 0) {
+    const articlePos = this.getArticlePos();
+    const positioningData = {
+      posGroup: [articlePos.x, articlePos.y, articlePos.z],
+      posRotation: articlePos.rotationY,
+      rootId: this.roots[0].root._id,
+      rootRelPos: [0,0,0],
+      rootRelRotation: 0
+    };
+    this.setRepositioningData(positioningData);
+  }
