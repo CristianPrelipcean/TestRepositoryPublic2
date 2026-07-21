99e06e3b5eb0303b2797f8c5950b2e162ec6295e
Vlad.Cobzaru@cadrantechnologies.eu <Vlad.Cobzaru@cadrantechnologies.eu>
2026-07-21 09:08:07 +0000

    Updated AdjustGroupScript script for module 'og'

--- vlad_cabinet_ts/modules/og.adjustgroup.ts
+++ vlad_cabinet_ts/modules/og.adjustgroup.ts
@@ -14,23 +14,23 @@
       }
 
       // dock on the floor
-      this.addDocking(lastDockedRoot, Dock.RightBottom, r1, Dock.LeftBottom, DockMode.StartStart, [0, 0, 0], 0);
+      this.addDocking(lastDockedRoot, Dock.RightBottom, r1, Dock.LeftBottom, DockMode.StartStart, [0, 2500, 0], 0);
 
       // lastDockedRoot = r1;
     }
 
-    const newArticle2 = this.createArticle('lilcab');
-    if (newArticle2) {
-      this.addRoots(newArticle2);
+    // const newArticle2 = this.createArticle('lilcab');
+    // if (newArticle2) {
+    //   this.addRoots(newArticle2);
 
-      let r2 = newArticle2[0].root;
-      if (r2 instanceof OD_M_shape01) {
-        r2.generateWorktop = false;
-        r2.height = 640;
-      }
+    //   let r2 = newArticle2[0].root;
+    //   if (r2 instanceof OD_M_shape01) {
+    //     r2.generateWorktop = false;
+    //     r2.height = 640;
+    //   }
 
       // dock on the wall
       // this.addDocking(lastDockedRoot, Dock.RightTop, r2, Dock.LeftTop, DockMode.StartStart, [0, 0, 0], 0);
-      this.addDocking(lastDockedRoot, Dock.RightBottom, r2, Dock.LeftBottom, DockMode.StartStart, [0, 2500, 0], 0);
-    }
+      // this.addDocking(lastDockedRoot, Dock.RightBottom, r2, Dock.LeftBottom, DockMode.StartStart, [0, 2500, 0], 0);
+    // }
   // }
