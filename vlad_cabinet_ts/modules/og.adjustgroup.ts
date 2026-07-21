8628113c653d2d9fa228d8721b2570c1e790072b
Vlad.Cobzaru@cadrantechnologies.eu <Vlad.Cobzaru@cadrantechnologies.eu>
2026-07-21 11:32:24 +0000

    Updated AdjustGroupScript script for module 'og'

--- vlad_cabinet_ts/modules/og.adjustgroup.ts
+++ vlad_cabinet_ts/modules/og.adjustgroup.ts
@@ -1,34 +1,30 @@
   logInfo("adjust with success from group orchestrator.");
 
   let lastDockedRoot = this.roots[0].root;
+  const newArticle = this.createArticle('lilcab');
+  if (newArticle) {
+    this.addRoots(newArticle);
 
-  // for (let i = 1; i <= 4; i++) {
-    const newArticle = this.createArticle('lilcab');
-    if (newArticle) {
-      this.addRoots(newArticle);
-
-      let r1 = newArticle[0].root;
-      if (r1 instanceof OD_M_shape01) {
-        r1.generateWorktop = false;
-        r1.height = 640;
-      }
-
-      // dock on the wall
-      this.addDocking(lastDockedRoot, Dock.RightTop, r1, Dock.LeftTop, DockMode.StartStart, [0, 0, 0], 0);
-
-      // lastDockedRoot = r1;
+    let r1 = newArticle[0].root;
+    if (r1 instanceof OD_M_shape01) {
+      r1.generateWorktop = false;
+      r1.height = 640;
     }
 
-    const newArticle2 = this.createArticle('lilcab');
-    if (newArticle2) {
-      this.addRoots(newArticle2);
+    // dock on the wall
+    this.addDocking(lastDockedRoot, Dock.RightTop, r1, Dock.LeftTop, DockMode.StartStart, [0, 0, 0], 0);
+  }
 
-      let r2 = newArticle2[0].root;
-      if (r2 instanceof OD_M_shape01) {
-        r2.generateWorktop = false;
-        r2.height = 640;
-      }
+  const newArticle2 = this.createArticle('lilcab');
+  if (newArticle2) {
+    this.addRoots(newArticle2);
 
-      // dock on the floor
-      this.addDocking(lastDockedRoot, Dock.RightBottom, r2, Dock.LeftBottom, DockMode.StartStart, [0, 0, 0], 0);
+    let r2 = newArticle2[0].root;
+    if (r2 instanceof OD_M_shape01) {
+      r2.generateWorktop = false;
+      r2.height = 640;
     }
+
+    // dock on the floor
+    this.addDocking(lastDockedRoot, Dock.RightBottom, r2, Dock.LeftBottom, DockMode.StartStart, [0, 0, 0], 0);
+  }
