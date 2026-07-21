a1e90ed5b7d75478378c8014fa1e5c07cb43d81e
Vlad.Cobzaru@cadrantechnologies.eu <Vlad.Cobzaru@cadrantechnologies.eu>
2026-07-21 05:52:48 +0000

    Updated AdjustGroupScript script for module 'og'

--- vlad_cabinet_ts/modules/og.adjustgroup.ts
+++ vlad_cabinet_ts/modules/og.adjustgroup.ts
@@ -1,16 +1,36 @@
   logInfo("adjust with success from group orchestrator.");
-  const newArticle = this.createArticle('tests');
-  if (newArticle) {
-    this.addRoots(newArticle);
 
-    let r1 = newArticle[0].root;
-    if (r1 instanceof OD_M_shape01) {
-      r1.height = 1548;
+  let lastDockedRoot = this.roots[0].root;
+
+  let lastAddedRoot;
+  let lastFloatingRoot;
+
+  for (let i = 0; i < 4; i++){
+    const newArticle = this.createArticle('lilcab');
+    if (newArticle) {
+      this.addRoots(newArticle);
+
+      let r1 = newArticle[0].root;
+      if (r1 instanceof OD_M_shape01) {
+        r1.height = 640;
+      }
+
+      //this.addDocking(this.roots[0].root, Dock.LeftBottom, r1, Dock.RightBottom);
+      if (i % 2 === 0) {
+        // dock on the floor
+        this.addDocking(lastDockedRoot, Dock.LeftBottom, r1, Dock.RightBottom, DockMode.StartStart, [0, 0, 0], 0);
+      }
+      else {
+        // dock on the wall
+        this.addDocking(lastDockedRoot, Dock.LeftBottom, r1, Dock.RightBottom, DockMode.StartStart, [0, 1800, 0], 0);
+      }
+      
+
+      lastDockedRoot = r1;
     }
+    else {
+      logError("article not found");
+    }
+  }
 
-    //this.addDocking(this.roots[0].root, Dock.LeftBottom, r1, Dock.RightBottom);
-    this.addDocking(this.roots[0].root, Dock.LeftBottom, r1, Dock.RightBottom, DockMode.StartStart, [0, 500, 0], 0);
-  }
-  else {
-    logError("article not found");
-  }
+  
