c83dd1a96839960201f49bec587a00cb6238f484
calin.chis@cadrantechnologies.eu <calin.chis@cadrantechnologies.eu>
2026-07-22 12:26:11 +0000

    Database imported from file 'Cut2Size_2026-07-03_11-41-15.0000000 (1).zip'

---
+++ cabinet_test_image/functions/check_articlethickness_getdropdownvalues_mod_thickness.ts
@@ -0,0 +1,3 @@
+check_ArticleThickness_getDropDownValues_mod_Thickness(attr: Checks.IArticleThickness_Attributes, selections: SelectionEntry_mod_Thickness[]): Checks.CheckDropDownEntry[]{
+    return [];
+}
---
+++ cabinet_test_image/modules/mr_part.calculatecontainermodule.ts
@@ -0,0 +1,22 @@
+  // Front View
+  if (this.mod_TypeElement_matrix.PartView == 'FrontView') {
+    const c = this.addOD_M_mrp_Part();
+    c.seal();
+    const width = c.mod_Width!;
+    const length = c.mod_Length!;
+    const thickness = c.mod_Thickness!;
+  }
+  else if(this.mod_TypeElement_matrix.PartView == 'ShelfView') {
+    const c = this.addOD_M_mrp_Part();
+    c.seal();
+    const width = c.mod_Width!;
+    const length = c.mod_Length!;
+    const thickness = c.mod_Thickness!;
+  }
+  else {
+    const c = this.addOD_M_mrp_Part();
+    c.seal();
+    const width = c.mod_Length!;
+    const length = c.mod_Thickness!;
+    const thickness = c.mod_Width!;
+  }
