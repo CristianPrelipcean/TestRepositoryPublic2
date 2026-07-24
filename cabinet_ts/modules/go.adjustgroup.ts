dd4a9836d4f4bb531833267eceb18fefd27173cf
heiko.reichstetter@homag.com <heiko.reichstetter@homag.com>
2026-07-24 04:56:51 +0000

    Updated AdjustGroupScript script for module 'go'

--- cabinet_ts/modules/go.adjustgroup.ts
+++ cabinet_ts/modules/go.adjustgroup.ts
@@ -4,14 +4,22 @@
   logInfo("z:" + this.getArticlePos().z);
   logInfo("rotationY:" + this.getArticlePos().rotationY);
 
-  if (this.roots.length > 0) {
-    const articlePos = this.getArticlePos();
-    const positioningData = {
-      posGroup: [articlePos.x, articlePos.y, articlePos.z],
-      posRotationY: articlePos.rotationY,
-      rootId: this.roots[0].root._id,
-      rootRelPos: [100,100,100],
-      rootRelRotationY: 45
-    };
-    this.setRepositioningData(positioningData);
+  // if (this.roots.length > 0) {
+  //   const articlePos = this.getArticlePos();
+  //   const positioningData = {
+  //     posGroup: [articlePos.x, articlePos.y, articlePos.z],
+  //     posRotationY: articlePos.rotationY,
+  //     rootId: this.roots[0].root._id,
+  //     rootRelPos: [100,100,100],
+  //     rootRelRotationY: 45
+  //   };
+  //   this.setRepositioningData(positioningData);
+  // }
+
+  let newArticle = this.createArticle("Article1");
+  if (newArticle) {
+    // Add the new article to this group
+    this.addRoot(newArticle[0]);
+  } else {
+    logError("Article not found");
   }
