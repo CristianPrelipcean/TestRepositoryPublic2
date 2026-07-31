b839c229b97c057099fa1aa8129e8fce2420d6b7
heiko.reichstetter@homag.com <heiko.reichstetter@homag.com>
2026-07-31 05:15:51 +0000

    Database imported from file 'cabinet_ts.zip'

--- hr_cabinet_ts/functions/check_test_getdropdownvalues_corpuscolor.ts
+++
@@ -1,3 +0,0 @@
-check_test_getDropDownValues_corpusColor(attr: Checks.Itest_Attributes, selections: SelectionEntry_corpusColor[]): Checks.CheckDropDownEntry[]{
-    return [];
-}
--- hr_cabinet_ts/functions/check_test_getdropdownvalues_doormaterial.ts
+++
@@ -1,3 +0,0 @@
-check_test_getDropDownValues_doorMaterial(attr: Checks.Itest_Attributes, selections: SelectionEntry_doorMaterial[]): Checks.CheckDropDownEntry[]{
-    return [];
-}
