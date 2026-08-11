9ee53a9726d402b8ed79beca5836e30ef813cb53
Vlad.Cobzaru@cadrantechnologies.eu <Vlad.Cobzaru@cadrantechnologies.eu>
2026-08-11 06:35:52 +0000

    Function 'check_aaa_getDropDownValues_corpusColor' changed.

--- vlad_cabinet_ts/functions/check_aaa_getdropdownvalues_corpuscolor.ts
+++ vlad_cabinet_ts/functions/check_aaa_getdropdownvalues_corpuscolor.ts
@@ -1,3 +1,4 @@
 check_aaa_getDropDownValues_corpusColor(attr: Checks.Iaaa_Attributes, selections: SelectionEntry_corpusColor[]): Checks.CheckDropDownEntry[]{
+    const result = GlobalFunc.globalFuncTestImport(2, 4);
     return [];
 }
