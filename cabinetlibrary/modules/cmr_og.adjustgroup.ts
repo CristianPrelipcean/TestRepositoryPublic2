b5022928cbe7097ea173342501c3ec3294b736f9
Vlad.Cobzaru@cadrantechnologies.eu <Vlad.Cobzaru@cadrantechnologies.eu>
2026-08-11 12:06:45 +0000

    Updated AdjustGroupScript script for module 'cmr_og'

--- cabinetlibrary/modules/cmr_og.adjustgroup.ts
+++ cabinetlibrary/modules/cmr_og.adjustgroup.ts
@@ -1,28 +1,37 @@
   logInfo("adjust with success from group orchestrator.");
 
-  let lastDockedRoot = this.roots[0].root;
-  const newArticle = this.createArticle('basic_Cabinet_006_fix_errors');
+  const articleName = "basic_Cabinet_006_fix_errors";
+  const thisRoot = this.roots[0].root;
+
+  const newArticle = this.createArticle(articleName);
   if (newArticle) {
     this.addRoots(newArticle);
 
     let r1 = newArticle[0].root;
-    // if (r1 instanceof OD_M_mr_StorageunitSingle) {
-
-    // }
+    if (r1 instanceof OD_M_mr_StorageunitSingle) {
+      r1.mod_Height = 900; // default is 850
+    }
 
     // dock on the wall
-    this.addDocking(lastDockedRoot, Dock.RightTop, r1, Dock.LeftTop, DockMode.StartStart, [0, 1500, 0], 0);
+    this.addDocking(thisRoot, Dock.RightTop, r1, Dock.LeftTop, DockMode.StartEnd, [0, 1500, 0], 0);
   }
 
-  const newArticle2 = this.createArticle('basic_Cabinet_006_fix_errors');
+  const newArticle2 = this.createArticle(articleName);
   if (newArticle2) {
     this.addRoots(newArticle2);
 
     let r2 = newArticle2[0].root;
-    // if (r2 instanceof OD_M_mr_StorageunitSingle) {
 
-    // }
+    // dock on the wall
+    this.addDocking(thisRoot, Dock.LeftTop, r2, Dock.LeftTop, DockMode.StartStart, [0, 1500, 0], 0);
+  }
+
+  const newArticle3 = this.createArticle(articleName);
+  if (newArticle3) {
+    this.addRoots(newArticle3);
+
+    let r3 = newArticle3[0].root;
 
     // dock on the floor
-    this.addDocking(lastDockedRoot, Dock.RightBottom, r2, Dock.LeftBottom, DockMode.StartStart, [0, 0, 0], 0);
+    this.addDocking(thisRoot, Dock.RightBottom, r3, Dock.LeftBottom, DockMode.EndEnd, [0, 0, 0], 0);
   }
