3958330906cbbd6cbc99fe205ff5d3e6e77038dc
calin.chis@cadrantechnologies.eu <calin.chis@cadrantechnologies.eu>
2026-08-12 11:05:43 +0000

    Added check for attributes: doorHeight, _articleId, testBoolSel

---
+++ cabinet_ts_scriptbug/functions/check_test2_getdropdownvalues_doorheight.ts
@@ -0,0 +1,3 @@
+check_test2_getDropDownValues_doorHeight(attr: Checks.Itest2_Attributes): Checks.CheckDropDownRange | undefined{
+    return undefined;
+}
---
+++ cabinet_ts_scriptbug/functions/check_test2_getdropdownvalues_testboolsel.ts
@@ -0,0 +1,3 @@
+check_test2_getDropDownValues_testBoolSel(attr: Checks.Itest2_Attributes, selections: SelectionEntry_testBoolSel[]): Checks.CheckDropDownEntry[]{
+    return [];
+}
