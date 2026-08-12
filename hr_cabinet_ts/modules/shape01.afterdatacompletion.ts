726cb6d22726b1013182f03f0faf7c7aa715d178
heiko.reichstetter@homag.com <heiko.reichstetter@homag.com>
2026-08-12 12:54:31 +0000

    Database imported from file 'hr_cabinet_ts.db'

--- hr_cabinet_ts/modules/shape01.afterdatacompletion.ts
+++ hr_cabinet_ts/modules/shape01.afterdatacompletion.ts
@@ -1,28 +1,28 @@
-  // Calculate the startPos
+// Calculate the startPos
 
-  let startPos = 0;
-  if (this.m.length > 0) {
-    let cm = this.m[0];
-    if (cm instanceof OD_M_singleDoor || cm instanceof OD_M_singleDrawer) {
-      startPos = cm.startPos ?? 0;
-    }
+let startPos = 0;
+if (this.m.length > 0) {
+  let cm = this.m[0];
+  if (cm instanceof OD_M_singleDoor || cm instanceof OD_M_singleDrawer) {
+    startPos = cm.startPos ?? 0;
+  }
+}
+
+const docking = this.addDockingInfo(Dock.LeftBottom, new Vector3(0,0,0), new Vector3(0,0,this.depth));
+this.addDockingInfo(Dock.RightBottom, new Vector3(this.width,0,0), new Vector3(this.width,0,this.depth));
+this.addDockingInfo(Dock.CollisionBox, new Vector3(0,0,0), new Vector3(this.width, this.height, this.depth));
+
+this.m.forEach(cm =>  {
+  if (cm instanceof OD_M_singleDoor) {
+    cm.startPos = startPos;
+    startPos += cm.frontHeight!;
   }
 
-  const docking = this.addDockingInfo(Dock.LeftBottom, new Vector3(0, 0, 0), new Vector3(0, 0, this.depth));
-  this.addDockingInfo(Dock.RightBottom, new Vector3(this.width, 0, 0), new Vector3(this.width, 0, this.depth));
-  this.addDockingInfo(Dock.CollisionBox, new Vector3(0, 0, 0), new Vector3(this.width, this.height, this.depth));
+  if (cm instanceof OD_M_singleDrawer) {
+    cm.startPos = startPos;
+    startPos += cm.height!;
+  }
+});
 
-  this.m.forEach(cm => {
-    if (cm instanceof OD_M_singleDoor) {
-      cm.startPos = startPos;
-      startPos += cm.frontHeight!;
-    }
-
-    if (cm instanceof OD_M_singleDrawer) {
-      cm.startPos = startPos;
-      startPos += cm.height!;
-    }
-  });
-
-  const roomContours = this.getRoomContours();
-  logInfo(JSON.stringify(roomContours));
+const roomContours = this.getRoomContours();
+logInfo(JSON.stringify(roomContours));
