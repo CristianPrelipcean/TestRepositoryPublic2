aa6f78185b2b7a22582d9983c9349dc1adc399bc
calin.chis@cadrantechnologies.eu <calin.chis@cadrantechnologies.eu>
2026-08-13 09:08:22 +0000

    Function 'check_test3_getDropDownValues_doorMaterial' changed.

--- cabinet_ts_scriptbug/functions/check_test3_getdropdownvalues_doormaterial.ts
+++ cabinet_ts_scriptbug/functions/check_test3_getdropdownvalues_doormaterial.ts
@@ -1,49 +1,49 @@
 check_test3_getDropDownValues_doorMaterial(attr: Checks.Itest3_Attributes, selections: SelectionEntry_doorMaterial[]): Checks.CheckDropDownEntry[]{
 type MaterialConstraint = { min: number; max: number };
 
-function isInRange(x: number, constraint: MaterialConstraint): boolean {
-    return constraint.min <= x && x <= constraint.max;
-}
+    function isInRange(x: number, constraint: MaterialConstraint): boolean {
+        return constraint.min <= x && x <= constraint.max;
+    }
 
-const heightConstraints = new Map<string, MaterialConstraint>([
-    ["wood",      { min: 2000, max: 3000 }],
-    ["steel",     { min: 2000, max: 2500 }],
-    ["glass",     { min: 1500, max: 2200 }],
-    ["aluminium", { min: 2500, max: 3000 }],
-]);
+    const heightConstraints = new Map<string, MaterialConstraint>([
+        ["wood",      { min: 2000, max: 3000 }],
+        ["steel",     { min: 2000, max: 2500 }],
+        ["glass",     { min: 1500, max: 2200 }],
+        ["aluminium", { min: 2500, max: 3000 }],
+    ]);
 
-const thicknessConstraints = new Map<string, MaterialConstraint>([
-    ["wood",      { min: 16, max: 25 }],
-    ["steel",     { min: 16, max: 19 }],
-    ["glass",     { min: 6,  max: 16 }],
-    ["aluminium", { min: 19, max: 25 }],
-]);
+    const thicknessConstraints = new Map<string, MaterialConstraint>([
+        ["wood",      { min: 16, max: 25 }],
+        ["steel",     { min: 16, max: 19 }],
+        ["glass",     { min: 6,  max: 16 }],
+        ["aluminium", { min: 19, max: 25 }],
+    ]);
 
-const result: Checks.CheckDropDownEntry[] = [];
+    const result: Checks.CheckDropDownEntry[] = [];
 
-for (const selection of selections) {
-    const key = selection.value ?? "";
-    const heightConstraint = heightConstraints.get(key);
-    const thicknessConstraint = thicknessConstraints.get(key);
+    for (const selection of selections) {
+        const key = selection.value ?? "";
+        const heightConstraint = heightConstraints.get(key);
+        const thicknessConstraint = thicknessConstraints.get(key);
 
-    const heightOk = heightConstraint !== undefined
-        && attr.doorHeight !== undefined
-        && isInRange(attr.doorHeight, heightConstraint);
+        const heightOk = heightConstraint !== undefined
+            && attr.doorHeight !== undefined
+            && isInRange(attr.doorHeight, heightConstraint);
 
-    const thicknessOk = thicknessConstraint !== undefined
-        && attr.thickness !== undefined
-        && isInRange(attr.thickness, thicknessConstraint);
+        const thicknessOk = thicknessConstraint !== undefined
+            && attr.thickness !== undefined
+            && isInRange(attr.thickness, thicknessConstraint);
 
-    const maxHeightToThicknessRatio = 2;
-    const ratioOk = attr.thickness !== undefined && attr.doorHeight !== undefined
-        && (attr.doorHeight / attr.thickness) <= maxHeightToThicknessRatio;
+        const maxHeightToThicknessRatio = 200;
+        const ratioOk = attr.thickness !== undefined && attr.doorHeight !== undefined
+            && (attr.doorHeight / attr.thickness) <= maxHeightToThicknessRatio;
 
-    const kind = (heightOk && thicknessOk && ratioOk)
-        ? Checks.CheckDropDownEntryKind.Normal
-        : Checks.CheckDropDownEntryKind.Conflicting;
+        const kind = (heightOk && thicknessOk && ratioOk)
+            ? Checks.CheckDropDownEntryKind.Normal
+            : Checks.CheckDropDownEntryKind.Conflicting;
 
-    result.push({ value: selection.value, kind });
-}
+        result.push({ value: selection.value, kind });
+    }
 
-return result;
+    return result;
 }
