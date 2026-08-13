a0ac339de24988058c751ed32c71ff50559bb08b
calin.chis@cadrantechnologies.eu <calin.chis@cadrantechnologies.eu>
2026-08-13 06:56:57 +0000

    Deleted check with id: 17

--- cabinet_ts_scriptbug/functions/check_test2_getdropdownvalues_doorheight.ts
+++
@@ -1,3 +0,0 @@
-check_test2_getDropDownValues_doorHeight(attr: Checks.Itest2_Attributes): Checks.CheckDropDownRange | undefined{
-    return undefined;
-}
--- cabinet_ts_scriptbug/functions/check_test2_getdropdownvalues_handleorientation.ts
+++
@@ -1,3 +0,0 @@
-check_test2_getDropDownValues_handleOrientation(attr: Checks.Itest2_Attributes, selections: SelectionEntry_handleOrientation[]): Checks.CheckDropDownEntry[]{
-    return [];
-}
