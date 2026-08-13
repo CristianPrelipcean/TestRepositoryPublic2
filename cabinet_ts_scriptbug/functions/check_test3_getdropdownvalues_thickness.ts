2295cc693cb4e3f0e0e292054fdd7d7c9b32c60d
calin.chis@cadrantechnologies.eu <calin.chis@cadrantechnologies.eu>
2026-08-13 09:09:01 +0000

    Function 'check_test3_getDropDownValues_thickness' changed.

--- cabinet_ts_scriptbug/functions/check_test3_getdropdownvalues_thickness.ts
+++ cabinet_ts_scriptbug/functions/check_test3_getdropdownvalues_thickness.ts
@@ -1,6 +1,6 @@
 check_test3_getDropDownValues_thickness(attr: Checks.Itest3_Attributes): Checks.CheckDropDownRange | undefined{
 
-    const thicknessConstraints = new Map<string, { min: number; max: number }>([
+ const thicknessConstraints = new Map<string, { min: number; max: number }>([
         ["wood",      { min: 16, max: 19 }],
         ["steel",     { min: 16, max: 22 }],
         ["glass",     { min: 6,  max: 16 }],
@@ -13,18 +13,8 @@ check_test3_getDropDownValues_thickness(attr: Checks.Itest3_Attributes): Checks.
         return undefined;
     }
 
-    let { min, max } = thicknessConstraint;
-
-    // clamp by door height: taller doors need more thickness
-    const minThicknessByHeight = 2;
-    if (attr.doorHeight !== undefined && attr.doorHeight > 0) {
-        const minByHeight = Math.ceil(attr.doorHeight / minThicknessByHeight);
-        min = Math.max(min, minByHeight);
-    }
-
-    if (min > max) {
-        return undefined;
-    }
+    const { min, max } = thicknessConstraint;
 
     return { min, max };
+
 }
