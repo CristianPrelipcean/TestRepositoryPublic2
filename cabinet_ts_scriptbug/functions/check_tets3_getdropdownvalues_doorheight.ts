a7618bf350d6abd80bfd3b3435d6e3b16a2d3bb1
calin.chis@cadrantechnologies.eu <calin.chis@cadrantechnologies.eu>
2026-08-13 06:55:21 +0000

    Deleted check with id: 15

--- cabinet_ts_scriptbug/functions/check_tets3_getdropdownvalues_doorheight.ts
+++
@@ -1,3 +0,0 @@
-check_tets3_getDropDownValues_doorHeight(attr: Checks.Itets3_Attributes): Checks.CheckDropDownRange | undefined{
-    return undefined;
-}
--- cabinet_ts_scriptbug/functions/check_tets3_getdropdownvalues_doormaterial.ts
+++
@@ -1,3 +0,0 @@
-check_tets3_getDropDownValues_doorMaterial(attr: Checks.Itets3_Attributes, selections: SelectionEntry_doorMaterial[]): Checks.CheckDropDownEntry[]{
-    return [];
-}
